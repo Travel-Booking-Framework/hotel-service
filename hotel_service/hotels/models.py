f6f9ba2a137bc1ba99d8547ba6643ce1b873e536
alireza-hme <hme.alireza@gmail.com>
2025-02-02 19:20:13 +0330

    Add hotel model

--- hotel_service/hotels/models.py
+++ hotel_service/hotels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    available_rooms = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.name} Hotel"
