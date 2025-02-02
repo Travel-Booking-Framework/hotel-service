c4be3ecf7ca1bc4c3b7397e8d184be9b440151ea
alireza-hme <hme.alireza@gmail.com>
2025-02-02 19:34:27 +0330

    Add price_per_night for model

--- hotel_service/hotels/models.py
+++ hotel_service/hotels/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Hotel(models.Model):
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     available_rooms = models.IntegerField()
 
 
