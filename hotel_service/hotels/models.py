b419549382e4df4dce8f415af4ec5f90631bfdde
alireza-hme <hme.alireza@gmail.com>
2025-02-03 00:31:44 +0330

    Add image model

--- hotel_service/hotels/models.py
+++ hotel_service/hotels/models.py
@@ -4,9 +4,17 @@ class Hotel(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    price_per_night = models.DecimalField(max_digits=10, decimal_places=2, min_value=0)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     available_rooms = models.IntegerField()
 
 
     def __str__(self):
         return f"{self.name} Hotel"
+
+
+class HotelImage(models.Model):
+    hotel = models.ForeignKey(Hotel, related_name='images', on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"Image for {self.hotel.name}"
--- hotel_service/hotels/serializers.py
+++ hotel_service/hotels/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
-from .models import Hotel
+from .models import Hotel, HotelImage
+
+class HotelImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HotelImage
+        fields = ['id', 'image_url']
 
 class HotelSerializer(serializers.ModelSerializer):
+    images = HotelImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Hotel
-        fields = '__all__'
+        fields = ['id', 'name', 'address', 'rating', 'images']
 
     def validate_price_per_night(self, value):
         if value < 0:
